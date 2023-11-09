dc542610b9482dcae7dc95a10fb59672872dfa84
MegSanta <megsantagata@gmail.com>
2023-11-08 21:00:09 -0800

    setting up codepath's API

--- app/src/main/java/com/example/morningmosaic/MainActivity.kt
+++ app/src/main/java/com/example/morningmosaic/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.morningmosaic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //views would need to match this
+        val goButton = findViewById<Button>(R.id.name_of_button)
+        goButton.setOnClickListener {
+            getContent()
+
+        }
+    }
+    private fun getContent() {
+
     }
 }
