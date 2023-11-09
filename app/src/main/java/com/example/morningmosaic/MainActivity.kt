59d0d26c716df163c06f4616eb74867c4c07b2ad
Yasaman Emami <emami.yasamann@gmail.com>
2023-11-09 14:36:01 -0800

    main layout updated

--- app/src/main/java/com/example/morningmosaic/MainActivity.kt
+++ app/src/main/java/com/example/morningmosaic/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.morningmosaic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Find the ImageView in the Toolbar layout
+        val toolbarImage = findViewById<ImageView>(R.id.toolbar_image)
         //test
     }
 }
